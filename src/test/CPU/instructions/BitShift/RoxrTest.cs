7c270650ba30e7c62ac4b0dd531bd7099ab8933f
Maximilien Noal <noal.maximilien@gmail.com>
2020-03-23 20:12:31 +0100

    RoxrTest
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/test/CPU/instructions/BitShift/RoxrTest.cs
@@ -0,0 +1,77 @@
+﻿
+
+namespace M68k.CPU.Instructions.BitShift
+{
+    using Miggy;
+    using Xunit;
+
+    public class RoxrTest : BasicSetup
+    {
+        public RoxrTest() : base()
+        {
+        }
+
+        [Fact]
+        public virtual void TestByte()
+        {
+            SetInstruction(0xe230);
+            model.CPU.SetDataRegister(0, -2023406616);
+            model.CPU.SetDataRegister(1, 4);
+            model.CPU.SetCCR((byte)0);
+            _ = model.CPU.Execute();
+            Assert.Equal(-2023406834, model.CPU.GetDataRegister(0));
+            Assert.False(model.CPU.IsSet(CpuCore.Z_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.V_FLAG));
+            Assert.True(model.CPU.IsSet(CpuCore.C_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.N_FLAG));
+            Assert.True(model.CPU.IsSet(CpuCore.X_FLAG));
+        }
+
+        [Fact]
+        public virtual void TestWord()
+        {
+            SetInstruction(0xe050);
+            model.CPU.SetDataRegister(0, -2023406815);
+            model.CPU.SetCCR((byte)0);
+            _ = model.CPU.Execute();
+            Assert.Equal(-2023407037, model.CPU.GetDataRegister(0));
+            Assert.False(model.CPU.IsSet(CpuCore.Z_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.V_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.C_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.N_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.X_FLAG));
+        }
+
+        [Fact]
+        public virtual void TestLong()
+        {
+            SetInstruction(0xe2b0);
+            model.CPU.SetDataRegister(0, -2023406815);
+            model.CPU.SetDataRegister(1, 18);
+            model.CPU.SetCCR((byte)0);
+            _ = model.CPU.Execute();
+            Assert.Equal(-1584356903, model.CPU.GetDataRegister(0));
+            Assert.False(model.CPU.IsSet(CpuCore.Z_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.V_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.C_FLAG));
+            Assert.True(model.CPU.IsSet(CpuCore.N_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.X_FLAG));
+        }
+
+        [Fact]
+        public virtual void TestMem()
+        {
+            SetInstruction(0xe4d0);
+            model.CPU.SetAddrRegister(0, 32);
+            model.MEM.Poke(32, -2023406815, Size.SizeLong);
+            model.CPU.SetCCR((byte)0);
+            _ = model.CPU.Execute();
+            Assert.Equal(0x43b24321, model.MEM.Peek(32, Size.SizeLong));
+            Assert.False(model.CPU.IsSet(CpuCore.Z_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.V_FLAG));
+            Assert.True(model.CPU.IsSet(CpuCore.C_FLAG));
+            Assert.False(model.CPU.IsSet(CpuCore.N_FLAG));
+            Assert.True(model.CPU.IsSet(CpuCore.X_FLAG));
+        }
+    }
+}
